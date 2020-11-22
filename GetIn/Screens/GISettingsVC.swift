12f33575e0c6be5a460942a68b98325573403d78
kru <s.krupe@gmail.com>
2020-11-22 15:40:01 +0300

    add new tab

---
+++ GetIn/Screens/GISettingsVC.swift
@@ -0,0 +1,19 @@
+//
+//  GISettingsVC.swift
+//  GetIn
+//
+//  Created by Sergei Krupenikov on 22.11.2020.
+//
+
+import UIKit
+
+class GISettingsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemTeal
+        title = "Settings"
+    }
+
+}
